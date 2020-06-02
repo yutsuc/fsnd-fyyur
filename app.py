42a1994a5421f8192c44636c922c3eb6c6ddad54
Jessica <yutsuc@gmail.com>
2020-06-02 13:00:25 -0700

    feat: Display Artists data from database

--- app.py
+++ app.py
@@ -283,18 +283,7 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-  return render_template('pages/artists.html', artists=data)
+  return render_template('pages/artists.html', artists=Artist.query.order_by("id").all())
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
